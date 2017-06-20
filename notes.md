c402a059abf9747288835cec74e93853e1fdc799
joshin-run <jahmsc@gmail.com>
2017-06-20 17:12:50 -0600

    Feat (Cards.js) Attempt to create populated cards from api data-unsuccessful

--- notes.md
+++ notes.md
@@ -1,3 +1,11 @@
 Start Time:  				End Time:
 
 Put any notes for us here.
+
+- ```render() {
+const url = `http://localhost:3008/people`;
+  $.ajax({
+    url: url
+  }).done(function(data) {
+  console.log('The returned object is:', data);
+  })```
--- src/Cards.js
+++ src/Cards.js
@@ -2,29 +2,57 @@ import React, { Component } from 'react';
 import './Card.css';
 import $ from 'jquery';
 
-class Card extends Component {
+class Cards extends Component {
+
   render() {
+
   const url = `http://localhost:3008/people`;
     $.ajax({
       url: url
     }).done(function(data) {
-    console.log('The returned object is:', data)
-    })
-    // console.log('outside of scope???', data);
+    console.log('The returned object is:', data);
+    let hero = {
+      name: data[0].name,
+      image: data[0].image,
+      birthYear: data[0].birth_year,
+      homeworld: data[0].homeworld
+    }
+    console.log(hero);
+
+    // function starWarsPeople (item, index) {
+    //   let nameBirthday = [item.name + " and " +  item.birth_year].join(" ");
+    //   return nameBirthday;
+    // }
+    // console.log(data);
+    // console.log(nameBirthday);
+    // function mappingPeopleInfo () {
+    //   console.log(data.map(starWarsPeople));
+    // }
+    // getFullName(item, index) {
+    // var fullname = [item.firstname,item.lastname].join(" ");
+    // return fullname;
+    // }
+
+// function myFunction() {
+//     document.getElementById("demo").innerHTML = persons.map(getFullName);
+// }
+
+  // console.log(data.map())
+})
 
     return (
       <div className='card'>
         <div className='card-content'>
-          	<div name={""} className='card-name'>Darth Vader</div>
-          	<img image={""} src='http://localhost:3008/darth_vader.jpg' alt='profile'/>
+          	<div className='card-name'>Darth Vader</div>
+          	<img src='http://localhost:3008/darth_vader.jpg' alt='profile'/>
             <p>
                 <span>Birthday:</span>
-                <span birthday={""}>41.9BBY</span>
+                <span>41.9BBY</span>
             </p>
             <p>
                 {/* Note that in order to get the homeworld's name, you have to get the planet name from a different endpoint than the people */}
                 <span>Homeworld:</span>
-                <span homePlanet={""}>Tatooine</span>
+                <span>Tatooine</span>
             </p>
         </div>
     </div>
@@ -33,4 +61,4 @@ class Card extends Component {
   }
 }
 
-export default Card;
+export default Cards;
